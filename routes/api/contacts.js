3fcf9693b8ed9e4890d4f9d649002292cbf194f7
AlyonaUdod <alyona.udod@ddaproduction.com>
2023-09-11 16:15:04 +0300

    add empty body validation

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,13 +2,15 @@ const express = require("express");
 
 const router = express.Router();
 
-const { joiSchema, favoriteJoiSchema } = require('../../schemas/contactSchemas.js')
-const { isEmptyBody } = require('../../middlewares/isEmptyBody');
-const {isIdExist} = require('../../middlewares/isIdExist.js')
-const { validateBody } = require('../../decorators/validateBody.js');
+const {
+  joiSchema,
+  favoriteJoiSchema
+} = require("../../schemas/contactSchemas.js");
+const { isEmptyBody } = require("../../middlewares/isEmptyBody");
+const { isIdExist } = require("../../middlewares/isIdExist.js");
+const { validateBody } = require("../../decorators/validateBody.js");
 
 const contactAddValidate = validateBody(joiSchema);
-
 
 const contactsController = require("../../controllers/contacts");
 
@@ -16,7 +18,19 @@ router.get("/", contactsController.getAllContacts);
 router.get("/:contactId", isIdExist, contactsController.getContactById);
 router.post("/", contactAddValidate, contactsController.postContact);
 router.delete("/:contactId", isIdExist, contactsController.deleteContact);
-router.put("/:contactId", isIdExist, isEmptyBody, contactAddValidate, contactsController.putContact);
-router.patch("/:contactId/favorite", isIdExist, validateBody(favoriteJoiSchema), contactsController.patchContact);
+router.put(
+  "/:contactId",
+  isIdExist,
+  isEmptyBody,
+  contactAddValidate,
+  contactsController.putContact
+);
+router.patch(
+  "/:contactId/favorite",
+  isIdExist,
+  isEmptyBody,
+  validateBody(favoriteJoiSchema),
+  contactsController.patchContact
+);
 
 module.exports = router;
